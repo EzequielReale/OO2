b38b425bddd7c0d3f8e00ddefb9fe3bbb646b805
Ezequiel Reale <88700625+EzequielReale@users.noreply.github.com>
2023-05-05 15:23:24 -0300

    Update ejercicio-19

--- Práctica/ejercicio19-PersonajesDeJuegosDeRol/src/main/java/OO2/ejercicio19_PersonajesDeJuegosDeRol/Personajes/Personaje.java
+++ Práctica/ejercicio19-PersonajesDeJuegosDeRol/src/main/java/OO2/ejercicio19_PersonajesDeJuegosDeRol/Personajes/Personaje.java
@@ -6,7 +6,7 @@ import OO2.ejercicio19_PersonajesDeJuegosDeRol.Armaduras.Armadura;
 import OO2.ejercicio19_PersonajesDeJuegosDeRol.Armas.Arma;
 import OO2.ejercicio19_PersonajesDeJuegosDeRol.Habilidades.Habilidad;
 
-public abstract class Personaje {
+public class Personaje {
 
 	private String nombre;
 	private int salud;
